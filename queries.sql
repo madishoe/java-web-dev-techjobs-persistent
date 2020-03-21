085ccfa55117a8fa4e4e3c365e86cfaa1e42a8a3
madisonshoemaker <madimadishoe@gmail.com>
2020-03-21 17:08:21 -0500

    Part 2 complete

--- queries.sql
+++ queries.sql
@@ -4,7 +4,9 @@ select * from skill;
 select * from employer;
 
 ## Part 2: Test it with SQL
-select * from employer where location='St. Louis City'
+select * from employer where location='St. Louis City';
+select * from employer where location='Kansas City, Mo';
+
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
