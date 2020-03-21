02c529f678ba4c9d84f3ec3591700a3c4965e277
madisonshoemaker <madimadishoe@gmail.com>
2020-03-21 17:06:33 -0500

    Part 2 SQL query not yet working

--- queries.sql
+++ queries.sql
@@ -1,8 +1,10 @@
 ## Part 1: Test it with SQL
 select * from job;
+select * from skill;
+select * from employer;
 
 ## Part 2: Test it with SQL
-select * from employer where location="St. Louis City"
+select * from employer where location='St. Louis City'
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
